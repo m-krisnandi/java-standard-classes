c7439f6efd6cf39d921b3737febde59b5f47951b
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-05-30 14:08:25 +0700

    math class

---
+++ src/classes/MathApp.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1000, 2000);
+        System.out.println(min); // 1000
+
+        var max = Math.max(1000, 2000);
+        System.out.println(max); // 2000
+
+        System.out.println(Math.PI); // 3.141592653589793
+    }
+}
